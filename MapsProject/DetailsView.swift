fdf75dd0a434f076d6f0fd9ecea3b372c82b6aa5
Segelll <gulbetekinberke@gmail.com>
2023-07-31 13:21:52 +0300

    uiless hourly

---
+++ MapsProject/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  MapsProject
+//
+//  Created by Eyüp Berke Gülbetekin on 31.07.2023.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
